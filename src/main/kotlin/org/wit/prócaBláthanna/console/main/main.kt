ccc9e9a3f148f681a16d987ec82ea5dd77b6bac0
Amie Abbotts <20079186@mail.wit.ie>
2021-10-08 10:50:47 +0100

    print simple menu in console

--- src/main/kotlin/org/wit/prócaBláthanna/console/main/main.kt
+++ src/main/kotlin/org/wit/prócaBláthanna/console/main/main.kt
@@ -1,2 +1,33 @@
 package org.wit.prócaBláthanna.console.main
 
+
+import mu.KotlinLogging
+
+
+private val logger = KotlinLogging.logger{}
+
+fun main(args: Array<String>) {
+    logger.info {"Launching Próca Bláthanna App"}
+    println("Próca Bláthanna App, for Mobile App Development")
+
+    fun menu() : Int {
+
+        var option : Int
+        var input: String? = null
+
+        println("Main Menu")
+        println(" 1. Add Flower")
+        println("-1. Exit")
+        println()
+        print("Enter a option")
+        input = readLine()!!
+        option = input.toInt()
+
+        return option
+
+
+
+
+    }
+
+}
