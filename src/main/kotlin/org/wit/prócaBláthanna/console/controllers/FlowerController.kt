f20ac2d5a7d91c7096f9f9ddc708c9b2b6168dac
Amie Abbotts <20079186@mail.wit.ie>
2021-10-11 18:11:15 +0100

    Added controller and helper files

---
+++ src/main/kotlin/org/wit/prócaBláthanna/console/controllers/FlowerController.kt
@@ -0,0 +1,84 @@
+package org.wit.prócaBláthanna.console.controllers
+
+import mu.KotlinLogging
+import org.wit.prócaBláthanna.console.models.FlowerModel
+import org.wit.prócaBláthanna.console.models.FlowerMemStore
+import org.wit.prócaBláthanna.console.views.FlowerView
+
+class FlowerController {
+
+    val flowers = FlowerMemStore()
+    val flowerView = FlowerView()
+    val logger = KotlinLogging.logger{}
+
+    init {
+        logger.info { "Launching Próca Bláthanna Console App" }
+    }
+
+    fun start() {
+        var input: Int
+
+        do {
+            input = menu()
+            when (input) {
+                1 -> add()
+                2 -> update()
+                3 -> list()
+                4 -> search()
+
+                -1 -> println("Exiting App")
+                else -> println("Invalid Option")
+            }
+            println()
+        } while (input != -1)
+        logger.info { "Shutting Down Próca Bláthanna Console App" }
+    }
+
+
+    fun menu() :Int { return flowerView.menu()}
+
+    fun add(){
+        var aFlower = FlowerModel()
+
+        if (flowerView.addFlowerData(aFlower))
+            flowers.create(aFlower)
+        else
+            logger.info("Placemark Not Added")
+    }
+
+    fun list() {
+        flowerView.listFlowers(flowers)
+    }
+
+    fun update() {
+
+        flowerView.listFlowers(flowers)
+        var searchId = flowerView.getId()
+        val aFlower = search(searchId)
+
+        if(aFlower != null) {
+            if(flowerView.updateFlowerData(aFlower)) {
+                flowers.update(aFlower)
+                flowerView.showFlower(aFlower)
+                logger.info("Flower Updated : [ $aFlower ]")
+            }
+            else
+                logger.info("Flower Not Updated")
+        }
+        else
+            println("Flower Not Updated...")
+    }
+
+    fun search() {
+        val aFlower = search(flowerView.getId())!!
+        flowerView.showFlower(aFlower)
+    }
+
+
+    fun search(id: Long) : FlowerModel? {
+        var foundFlower = flowers.findOne(id)
+        return foundFlower
+    }
+
+
+}
--- src/main/kotlin/org/wit/prócaBláthanna/console/main/main.kt
+++ src/main/kotlin/org/wit/prócaBláthanna/console/main/main.kt
@@ -2,6 +2,7 @@ package org.wit.prócaBláthanna.console.main
 
 
 import mu.KotlinLogging
+import org.wit.prócaBláthanna.console.controllers.FlowerController
 import org.wit.prócaBláthanna.console.models.FlowerMemStore
 import org.wit.prócaBláthanna.console.models.FlowerModel
 import org.wit.prócaBláthanna.console.views.FlowerView
@@ -14,24 +15,7 @@ val flowers = FlowerMemStore()
 val flowerView = FlowerView()
 
 fun main(args: Array<String>) {
-    logger.info { "Launching PrócaBláthanna Console App" }
-
-
-    var input: Int
-
-    do {
-        input = flowerView.menu()
-        when (input) {
-            1 -> addFlower()
-            2 -> updateFLower()
-            3 -> flowerView.listFlowers(flowers)
-            4 -> searchFlower()
-            -1 -> println("Exiting App")
-            else -> println("Invalid Option")
-        }
-        println()
-    } while (input != -1)
-    logger.info { "Shutting Down PrócaBláthanna Console App" }
+  FlowerController().start()
 }
 
 
