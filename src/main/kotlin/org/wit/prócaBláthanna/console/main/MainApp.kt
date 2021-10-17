2c8f958451fc00cbf9a3faabcbf3d7b808564588
Amie Abbotts <20079186@mail.wit.ie>
2021-10-17 02:10:28 +0100

    Tornado configured and simple button implemented to check build is correct

---
+++ src/main/kotlin/org/wit/prócaBláthanna/console/main/MainApp.kt
@@ -0,0 +1,8 @@
+package org.wit.prócaBláthanna.console.main
+
+import org.wit.prócaBláthanna.console.views.FlowerView
+import org.wit.prócaBláthanna.console.views.MyView
+import tornadofx.*
+
+
+class MainApp : App(MyView::class)
---
+++ src/main/kotlin/org/wit/prócaBláthanna/console/views/MyView.kt
@@ -0,0 +1,9 @@
+package org.wit.prócaBláthanna.console.views
+import tornadofx.*
+
+class MyView: View() {
+    override val root =
+        button{
+            ("press me")
+        }
+    }
